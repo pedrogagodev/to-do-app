08eae0a46b1145fc941c2adc528ba6caad45d2f4
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-23 13:53:35 -0300

    :q

---
+++ src/components/Elements/Empty.tsx
@@ -0,0 +1,15 @@
+import Clipboard from '../../assets/Clipboard.svg'
+
+export default function Empty() {
+    return (
+        <div className="flex flex-col items-center justify-center rounded-lg border-t border-my-gray-400 text-base text-my-gray-300">
+          <img src={Clipboard} alt="" className="mb-4 mt-16 h-14 w-14" />
+          <div className="mb-16 text-center">
+            <p className="font-bold">
+              You still don't have any registered tasks
+            </p>
+            <p>Create tasks and organize your to-do items</p>
+          </div>
+        </div>
+    )
+}
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,34 +1,39 @@
-import Clipboard from '../assets/Clipboard.svg'
-import Plus from '../assets/plus.svg'
-
+import Empty from './Elements/Empty';
+import Plus from '../assets/plus.svg';
 
 export function Page() {
-    return (
-        <div className="bg-my-gray-600 w-full flex flex-col items-center grow font-inter">
-            <div className="flex gap-2 h-14  w-content-w relative z-10 -top-7">
-                <form action="" method="get" className='w-full'>
-                    <input type="text" placeholder="Add a new task" className="h-14 rounded-lg bg-my-gray-500 w-full font-inter pl-4"/>
-                </form>
-                    <button className="flex gap-2 bg-blue-dark rounded-lg h-14 w-24 font-inter py-4 pl-4 pr-8 text-my-gray-100 font-bold">Create <img src={Plus} alt="" className='self-center'/></button>
-            </div>  
+  return (
+    <div className="flex w-full grow flex-col items-center bg-my-gray-600 font-inter">
+      <div className="relative -top-7 z-10 flex h-14 w-content-w gap-2">
+        <form action="" method="get" className="w-full">
+          <input
+            type="text"
+            placeholder="Add a new task"
+            className="h-14 w-full rounded-lg bg-my-gray-500 pl-4 font-inter"
+          />
+        </form>
+        <button className="flex h-14 w-24 gap-2 rounded-lg bg-blue-dark py-4 pl-4 pr-8 font-inter font-bold text-my-gray-100">
+          Create <img src={Plus} alt="" className="self-center" />
+        </button>
+      </div>
 
-            <div className="mt-16 w-content-w h-72">
-                <div className="flex justify-between mb-6 font-bold">
-                    <p className="flex gap-2 text-blue">Tasks created
-                        <span className="text-white bg-my-gray-400 px-2 rounded-full">0</span>
-                    </p>
-                    <p className="flex gap-2 text-purple">Completed
-                        <span className="text-white bg-my-gray-400 px-2 rounded-full">0</span>
-                    </p>
-                </div>
-                <div className="flex flex-col justify-center items-center border-t border-my-gray-400 rounded-lg text-my-gray-300 text-base">
-                    <img src={Clipboard} alt="" className='w-14 h-14 mt-16 mb-4'/>
-                    <div className='mb-16 text-center'>
-                        <p className='font-bold'>You still don't have any registered tasks</p>
-                        <p>Create tasks and organize your to-do items</p>
-                    </div>
-                </div>
-            </div>
+      <div className="mt-16 h-72 w-content-w">
+        <div className="mb-6 flex justify-between font-bold">
+          <p className="flex gap-2 text-blue">
+            Tasks created
+            <span className="rounded-full bg-my-gray-400 px-2 text-white">
+              0
+            </span>
+          </p>
+          <p className="flex gap-2 text-purple">
+            Completed
+            <span className="rounded-full bg-my-gray-400 px-2 text-white">
+              0
+            </span>
+          </p>
         </div>
-    )
+        <Empty />
+      </div>
+    </div>
+  );
 }
