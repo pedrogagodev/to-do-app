ca9711e6da379f9ca3a5bb1c504f87197d43e9e7
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-23 22:06:24 -0300

    Adds the functionality to create tasks

--- src/components/Elements/Task.tsx
+++ src/components/Elements/Task.tsx
@@ -1,15 +1,21 @@
 import Trash from '../../assets/Trash.svg'
 import { Check } from '@phosphor-icons/react'
+import { ITask } from '../Page'
 
-export function Task() {
+interface Props {
+    data: ITask
+}
+
+export function Task({ data }: Props) {
+
     return (
         <div className="flex rounded-lg w-content-w bg-my-gray-400 gap-3 p-4">
             <label htmlFor="checkbox" className='flex gap-5'>
                 <input type="checkbox" name="" id="" className='absolute appearance-none w-7 h-7 border-2 rounded-full border-blue checked:border-purple-dark checked:bg-purple-dark mt-2'/>
                 <span className='pl-1 pt-3'><Check size={18} color='white'/></span>
-                <span className='text-my-gray-100 '>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+                <span className='text-my-gray-100 '>{data.content}</span>
             </label>
            <img src={Trash} alt="" className='h-4 w-4 mt-4'/>
         </div>
     )
-}
+}   
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,16 +1,51 @@
 import { Empty } from './Elements/Empty'
 import { Task } from './Elements/Task'
 import Plus from '../assets/plus.svg'
+import { useState } from 'react'
 
+export interface ITask {
+    id: number,
+    content: string,
+    isChecked: boolean
+  }
+  
 
 export function Page() {
+     const [tasks, setTasks] = useState<ITask[]>([])
+      const [inputValue, setInputValue] = useState('')
+    
+      function handleAddTask() {
+        if (!inputValue) return
+    
+        const newTask: ITask = {
+          id: new Date().getTime(),
+          content: inputValue,
+          isChecked: false,
+        }
+    
+        setTasks((state) => [...state, newTask])
+        setInputValue('')
+      }
+
+      console.log(tasks)
+
+      
+    
     return (
         <div className="bg-my-gray-600 w-full flex flex-col items-center grow font-inter">
             <div className="flex gap-2 h-14  w-content-w relative z-10 -top-7">
                 <form action="" method="get" className='w-full'>
-                    <input type="text" placeholder="Add a new task" className="h-14 rounded-lg bg-my-gray-500 w-full font-inter pl-4"/>
+                    <input type="text" 
+                    placeholder="Add a new task" 
+                    className="h-14 rounded-lg bg-my-gray-500 w-full font-inter pl-4"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                    />
                 </form>
-                    <button className="flex gap-2 bg-blue-dark rounded-lg h-14 w-24 font-inter py-4 pl-4 pr-8 text-my-gray-100 font-bold">Create <img src={Plus} alt="" className='self-center'/></button>
+                    <button className="flex gap-2 bg-blue-dark rounded-lg h-14 w-24 font-inter py-4 pl-4 pr-8 text-my-gray-100 font-bold"
+                    onClick={handleAddTask}
+                    >Create <img src={Plus} alt="" className='self-center'/>
+                    </button>
             </div>  
 
             <div className="mt-16 w-content-w h-72">
@@ -22,8 +57,13 @@ export function Page() {
                         <span className="text-white bg-my-gray-400 px-2 rounded-full">0</span>
                     </p>
                 </div>
-              <Task /> 
+                <div>
+                    {tasks.map((task) => {
+                       return <Task key={task.id} data={task}/>
+                    })}
+                </div>
             </div>
         </div>
     )
 }
+
