372b288db78f8eb95ddc6f0c728da161cf62faeb
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-24 01:31:46 -0300

    feat: adds the feature of changing style when clicking on the task 'check' button

--- src/components/Elements/Task.tsx
+++ src/components/Elements/Task.tsx
@@ -5,9 +5,14 @@ import { ITask } from '../Page'
 interface Props {
     data: ITask
     removeTask: (id: number) => void
+    toggleTaskStatus: ({id, value}: {id: number; value: boolean}) => void
 }
 
-export function Task({ data, removeTask }: Props) {
+export function Task({ data, removeTask, toggleTaskStatus }: Props) {
+
+    function handleTaskStatus() {
+        toggleTaskStatus({id: data.id, value: !data.isChecked})
+    }
 
     function handleRemove() {
         removeTask(data.id)
@@ -16,9 +21,9 @@ export function Task({ data, removeTask }: Props) {
     return (
         <div className="flex rounded-lg w-content-w bg-my-gray-400 gap-3 p-4">
             <label htmlFor="checkbox" className='flex gap-5'>
-                <input type="checkbox" name="" id="" className='absolute appearance-none w-7 h-7 border-2 rounded-full border-blue checked:border-purple-dark checked:bg-purple-dark mt-2'/>
+                <input onClick={handleTaskStatus} type="checkbox" name="" id="" className='absolute appearance-none w-7 h-7 border-2 rounded-full border-blue checked:border-purple-dark checked:bg-purple-dark mt-2'/>
                 <span className='pl-1 pt-3'><Check size={18} color='white'/></span>
-                <span className='text-my-gray-100 '>{data.content}</span>
+                <span className={`text-${data.isChecked? 'my-gray-300 line-through': 'my-gray-100'}`}>{data.content}</span>
             </label>
            <img src={Trash} onClick={handleRemove} alt=""className='h-4 w-4 mt-4'/>
         </div>
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -33,6 +33,19 @@ export function Page() {
         setTasks(filteredTasks)
     }
 
+    function handleToggleStatus({ id, value }: { id: number; value: boolean }) {
+        const updatedTasks = tasks.map((task) => {
+            if (task.id === id) {
+                return {...task, isChecked: value}
+            }
+
+            return {...task}
+        })
+
+        setTasks(updatedTasks)
+    }
+
+
     return (
         <div className="bg-my-gray-600 w-full flex flex-col items-center grow font-inter">
             <div className="flex gap-2 h-14  w-content-w relative z-10 -top-7">
@@ -53,15 +66,15 @@ export function Page() {
             <div className="mt-16 w-content-w h-72">
                 <div className="flex justify-between mb-6 font-bold">
                     <p className="flex gap-2 text-blue">Tasks created
-                        <span className="text-white bg-my-gray-400 px-2 rounded-full">0</span>
+                        <span className="text-white bg-my-gray-400 px-2 rounded-full">{tasks.length}</span>
                     </p>
                     <p className="flex gap-2 text-purple">Completed
-                        <span className="text-white bg-my-gray-400 px-2 rounded-full">0</span>
+                        <span className="text-white bg-my-gray-400 px-2 rounded-full">completed of {tasks.length}</span>
                     </p>
                 </div>
                 <div>
                     {tasks.map((task) => {
-                       return <Task key={task.id} data={task} removeTask={handleRemoveTask}/>
+                       return <Task key={task.id} data={task} removeTask={handleRemoveTask} toggleTaskStatus={handleToggleStatus}/>
                     })}
                 </div>
             </div>
