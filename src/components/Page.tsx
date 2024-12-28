f52474ba76723ee7b4fff022e308fa4432c818c8
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-27 22:16:50 -0300

    feat: Adds 'enter' as form submit and add completed task counter

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,7 @@
 import { Task } from './Elements/Task'
 import Plus from '../assets/plus.svg'
 import { useState } from 'react'
+import { Empty } from './Elements/Empty'
 
 export interface ITask {
     id: number,
@@ -12,6 +13,8 @@ export interface ITask {
 export function Page() {
     const [tasks, setTasks] = useState<ITask[]>([])
     const [inputValue, setInputValue] = useState('')
+
+    const tasksChecked = tasks.filter(item => item.isChecked === true)
     
     function handleAddTask() {
         if (!inputValue) return
@@ -25,6 +28,11 @@ export function Page() {
         setTasks((state) => [...state, newTask])
         setInputValue('')
       }
+
+    function handleOnSubmit (e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        handleAddTask()
+    }
 
     function handleRemoveTask(id: number) {
         const filteredTasks = tasks.filter((task) => task.id !== id)
@@ -48,7 +56,7 @@ export function Page() {
     return (
         <div className="bg-my-gray-600 w-screen flex flex-col items-center grow font-inter">
             <div className="flex gap-2 h-14  w-content-w relative z-10 top-7">
-                <form action="" method="get" className='w-full'>
+                <form onSubmit={handleOnSubmit} method="get" className='w-full'>
                     <input type="text" 
                     placeholder="Add a new task" 
                     className="h-14 rounded-lg bg-my-gray-500 w-full font-inter pl-4 text-white"
@@ -63,19 +71,19 @@ export function Page() {
                     <img src={Plus} alt="" className='self-center absolute right-0 pr-1.5'/>
             </div>  
 
-            <div className="mt-16 min-h-72 flex-1">
+            <div className="mt-16 w-content-w min-h-72 flex-1">
                 <div className="flex justify-between mb-6 font-bold">
                     <p className="flex gap-2 text-blue">Tasks created
                         <span className="text-white bg-my-gray-400 px-2 rounded-full">{tasks.length}</span>
                     </p>
                     <p className="flex gap-2 text-purple">Completed
-                        <span className="text-white bg-my-gray-400 px-2 rounded-full">completed of {tasks.length}</span>
+                        <span className="text-white bg-my-gray-400 px-2 rounded-full">{tasksChecked.length != 0? `${tasksChecked.length} of ${tasks.length}`: '0'}</span>
                     </p>
                 </div>
                 <div>
-                    {tasks.map((task) => {
+                    {tasks.length != 0? (tasks.map((task) => {
                        return <Task key={task.id} data={task} removeTask={handleRemoveTask} toggleTaskStatus={handleToggleStatus}/>
-                    })}
+                    })): <Empty />}
                 </div>
             </div>
         </div>
