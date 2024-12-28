4bff704a64630ef60c9636a2a20d21f515ac60b0
Pedro Oliveira <pedrogagodev@gmail.com>
2024-12-27 21:54:12 -0300

    Make some adjustments to the page styling

--- src/components/Elements/Task.tsx
+++ src/components/Elements/Task.tsx
@@ -19,13 +19,12 @@ export function Task({ data, removeTask, toggleTaskStatus }: Props) {
     }
 
     return (
-        <div className="flex rounded-lg w-content-w bg-my-gray-400 gap-3 p-4">
-            <label htmlFor="checkbox" className='flex gap-5'>
-                <input onClick={handleTaskStatus} type="checkbox" name="" id="" className='absolute appearance-none w-7 h-7 border-2 rounded-full border-blue checked:border-purple-dark checked:bg-purple-dark mt-2'/>
-                <span className='pl-1 pt-3'><Check size={18} color='white'/></span>
-                <span className={`text-${data.isChecked? 'my-gray-300 line-through': 'my-gray-100'}`}>{data.content}</span>
+        <div className="flex rounded-lg w-content-w bg-my-gray-400 gap-3 p-4 mb-3 justify-between break-all">
+            <label htmlFor="checkbox" className='flex gap-5 '>
+                <input onClick={handleTaskStatus} type="checkbox" name="" id="" className='absolute appearance-none w-7 h-7 border-2 rounded-full border-blue checked:border-purple-dark checked:bg-purple-dark'/>
+                <span className={`text-${data.isChecked? 'my-gray-300 line-through': 'my-gray-100'} ml-11`}>{data.content}</span>
             </label>
-           <img src={Trash} onClick={handleRemove} alt=""className='h-4 w-4 mt-4'/>
+           <img src={Trash} onClick={handleRemove} alt=""className='h-6 w-5 mt-1 hover:cursor-pointer'/>
         </div>
     )
 }   
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,3 @@
-import { Empty } from './Elements/Empty'
 import { Task } from './Elements/Task'
 import Plus from '../assets/plus.svg'
 import { useState } from 'react'
@@ -47,23 +46,24 @@ export function Page() {
 
 
     return (
-        <div className="bg-my-gray-600 w-full flex flex-col items-center grow font-inter">
-            <div className="flex gap-2 h-14  w-content-w relative z-10 -top-7">
+        <div className="bg-my-gray-600 w-screen flex flex-col items-center grow font-inter">
+            <div className="flex gap-2 h-14  w-content-w relative z-10 top-7">
                 <form action="" method="get" className='w-full'>
                     <input type="text" 
                     placeholder="Add a new task" 
-                    className="h-14 rounded-lg bg-my-gray-500 w-full font-inter pl-4"
+                    className="h-14 rounded-lg bg-my-gray-500 w-full font-inter pl-4 text-white"
                     onChange={(e) => setInputValue(e.target.value)}
                     value={inputValue}
                     />
                 </form>
-                    <button className="flex gap-2 bg-blue-dark rounded-lg h-14 w-24 font-inter py-4 pl-4 pr-8 text-my-gray-100 font-bold"
+                    <button className="flex gap-2 bg-blue-dark rounded-lg h-14 w-24 font-inter py-4 pl-4 pr-10 text-my-gray-100 font-bold"
                     onClick={handleAddTask}
-                    >Create <img src={Plus} alt="" className='self-center'/>
+                    >Create
                     </button>
+                    <img src={Plus} alt="" className='self-center absolute right-0 pr-1.5'/>
             </div>  
 
-            <div className="mt-16 w-content-w h-72">
+            <div className="mt-16 min-h-72 flex-1">
                 <div className="flex justify-between mb-6 font-bold">
                     <p className="flex gap-2 text-blue">Tasks created
                         <span className="text-white bg-my-gray-400 px-2 rounded-full">{tasks.length}</span>
